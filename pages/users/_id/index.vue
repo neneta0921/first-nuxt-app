9253997c3f880fd4656d8a3bc5afedd5599db87b
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2021-02-09 14:13:20 +0900

    validating parameters

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -1,3 +1,12 @@
 <template>
   <h1>A single user, with ID: {{ $route.params.id }}</h1>
 </template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  validate(data) {
+    return /^\d+$/.test(data.params.id);
+  },
+});
+</script>
